f9221e5136e6531a1721e95a078ddd53f174531e
username <fruttys@gmail.com>
2022-08-18 20:05:17 +0300

    add cards and delete

---
+++ index.js
@@ -0,0 +1,64 @@
+const express = require("express");
+require("dotenv").config();
+const cors = require("cors");
+const { MongoClient, ObjectId } = require("mongodb");
+const app = express();
+app.use(express.json());
+app.use(cors());
+const port = process.env.PORT || 8080;
+const uri = process.env.CONNECTION;
+const client = new MongoClient(uri);
+const DB = "membership_management";
+
+// const usersCollection = "users";
+const membershipsCollection = "memberships";
+
+app.get("/memberships", async (req, res) => {
+  try {
+    const con = await client.connect();
+
+    const data = await con
+      .db(DB)
+      .collection(membershipsCollection)
+      .find()
+      .toArray();
+    await con.close();
+    return res.send(data);
+  } catch (error) {
+    res.status(500).send({ error });
+  }
+});
+
+app.post("/memberships", async (req, res) => {
+  try {
+    const con = await client.connect();
+
+    const data = await con
+      .db(DB)
+      .collection(membershipsCollection)
+      .insertOne(req.body);
+
+    await con.close();
+    return res.send(data);
+  } catch (error) {
+    res.status(500).send({ error });
+  }
+});
+
+app.delete("/memberships/:id", async (req, res) => {
+  try {
+    const con = await client.connect();
+
+    const data = await con
+      .db(DB)
+      .collection(membershipsCollection)
+      .deleteOne({ _id: ObjectId(req.params.id) });
+
+    await con.close();
+    return res.send(data);
+  } catch (error) {
+    res.status(500).send({ error });
+  }
+});
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
